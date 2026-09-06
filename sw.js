b38d24990ed5bb0037816ca2d7ca25dc85ee0be3
0322shiun-blip <0322shiun@gmail.com>
2026-09-06 11:16:16 +0800

    Update service worker cache for V10

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="todo-pwa-v8";
-const ASSETS=["./","index.html","style.css","app.js?v=9","manifest.json","icon.svg"];
+const CACHE="todo-pwa-v9";
+const ASSETS=["./","index.html","style.css","app.js?v=10","manifest.json","icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
